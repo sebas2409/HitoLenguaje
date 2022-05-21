f1aafedf5f738463db8b2af4fa0272ebca4537d5
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2022-05-21 16:31:36 +0200

    notas anadidas

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ window.onload = ()=>{
     getListadoAlumnos()
 }
 
+
 function getListadoAlumnos() {
     fetch("https://apirest-estudiantes1dam.herokuapp.com",{ method: 'GET'})
         .then(response => response.json())
-        .then(data =>mostrarAlumnos(data))
+        .then(data =>
+            mostrarAlumnos(data)
+        )
 }
 
 function mostrarAlumnos(json) {
@@ -16,7 +19,13 @@ function mostrarAlumnos(json) {
             fila.innerHTML = "<td>" + json[selectKey][i]["id"] + "</td>" +
                 "<td>" + json[selectKey][i]["nombre"] + "</td>" +
                 "<td>" + json[selectKey][i]["curso"] + "</td>" +
-                "<td>" + json[selectKey][i]["notas"][0]["asignatura"] + "</td>" +
+                "<td>" + "<li>"+json[selectKey][i]["notas"][0]["asignatura"]+" - "+json[selectKey][i]["notas"][0]["nota"]+"</li>"  +
+                "<li>"+json[selectKey][i]["notas"][1]["asignatura"]+" - "+json[selectKey][i]["notas"][1]["nota"]+"</li>"+
+                "<li>"+json[selectKey][i]["notas"][2]["asignatura"]+" - "+json[selectKey][i]["notas"][2]["nota"]+"</li>"+
+                "<li>"+json[selectKey][i]["notas"][3]["asignatura"]+" - "+json[selectKey][i]["notas"][3]["nota"]+"</li>"+
+                "<li>"+json[selectKey][i]["notas"][4]["asignatura"]+" - "+json[selectKey][i]["notas"][4]["nota"]+"</li>"+
+                "<li>"+json[selectKey][i]["notas"][5]["asignatura"]+" - "+json[selectKey][i]["notas"][5]["nota"]+"</li>"+
+                "</td>"
                 select.appendChild(fila);
         }
     }
